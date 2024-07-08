91dc4ea29cef94ff8938e90d70d2baf3fdbe24e0
sahland <sahland@mail.ru>
2024-07-08 13:43:22 +0300

    create stats system

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Start()
+    {
+        maxHealth = 100;
+    }
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,28 @@
+public class PlayerStats : CharacterStats
+{
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    private void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        PlayerManager.instance.KillPlayer();
+    }
+}
