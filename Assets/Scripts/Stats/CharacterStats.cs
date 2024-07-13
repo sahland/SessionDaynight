2ee07823cffe930055390b68c8a42b7b3bd0e3be
sahland <sahland@mail.ru>
2024-07-13 19:07:31 +0300

    update stats

--- Assets/Scripts/Stats/CharacterStats.cs
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -8,6 +8,8 @@ public class CharacterStats : MonoBehaviour
     public Stat damage;
     public Stat armor;
 
+    public event System.Action<int, int> OnHealthChanged;
+
     private void Start()
     {
         maxHealth = 100;
@@ -33,6 +35,11 @@ public class CharacterStats : MonoBehaviour
 
         currentHealth -= damage;
         Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if (OnHealthChanged != null)
+        {
+            OnHealthChanged(maxHealth, currentHealth);
+        }
 
         if (currentHealth <= 0)
         {
